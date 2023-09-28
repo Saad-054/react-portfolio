d1f46ce4c61c7ee9bb371afc4e0abb12507d5a0a
Saad-054 <saad.siddiqui@msn.com>
2023-09-28 23:03:40 +0100

    footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+// Footer.js
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <a href="#">Terms of Service</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Copyright Notice</a>
+      </div>
+      <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
